3c06eb3536e8cc0bae1af9e7e1fdd32bbe001e43
zhouyw <779736178@qq.com>
2020-06-06 20:50:24 +0800

    Demo提交

---
+++ demo/src/test/java/dz/sprintboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.sprintboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
